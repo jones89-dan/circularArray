b73c122787eea16053ff853d0b6cb378af391128
Daniel <jones.89.dan@gmail.com>
2021-10-31 11:51:57 -0700

    Added Javadoc documentation and comments

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -24,39 +24,64 @@ public class ArrayQueue<T> implements QueueInterface<T>
     //implement your methods with Javadoc comments here
     //again, you are NOT ALLOWED to add more instance variables
     
+    /**
+    * insert method checks for full queue, if not adds to the rear of the queue.
+    * 
+    * @param element - generic item to be added to the queue.
+    */
     public void insert(T element) throws QueueOverflowException
     {
+        // checks if queue is full and throws exception if true
         if (isFull())
             throw new QueueOverflowException ("Queue overflow"); 
-            
+        
+        // checks rear and wraps to the first position, else increments rear
         if (rear == MAX - 1) 
-            rear = 0; //wrap queue round to beginning of array 
+            rear = 0; 
         else 
             rear = rear + 1; 
-            
+        
+        // insert element at the rear of queue
         elements[rear] = element;
     }
     
+    /**
+    * remove method checks for empty queue, if not removes first item.
+    * 
+    * @return temp - item removed from end of the queue.
+    */
     public T remove() throws QueueUnderflowException
     {
+        // checks if queue and throws exception if true
         if (isEmpty())
             throw new QueueUnderflowException ("Queue underflow"); 
        
+        // checks front and wraps to the first position, else increments front
         if (front == MAX - 1)
             front = 0;
         else
             front = front + 1; 
         
+        // item to be removed and returns the value
         T temp = elements[front];
-        
         return temp;
     }
     
+    /**
+    * isEmpty checks for empy queue.
+    * 
+    * @return boolean - True if rear and front are at the same position in array.
+    */
     public Boolean isEmpty()
     {
         return rear == front;
     }
     
+    /**
+    * isFull  checks for full queue.
+    * 
+    * @return boolean - True if we updated rear and front is equal to rear.
+    */
     public Boolean isFull()
     {
         return rear + 1 == front;
